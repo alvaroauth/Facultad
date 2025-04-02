5045c9bc3e190dd15cf5b3001addaeba14667f94
alvaroauth <alvarofranciscoaufmuth@gmail.com>
2025-04-01 21:25:57 -0300

    UNLP Informatica 2025

--- Segundo/Ayed/src/Practica1/Ejercicio9/Ejercicio9.java
+++ Segundo/Ayed/src/Practica1/Ejercicio9/Ejercicio9.java
@@ -10,20 +10,22 @@ public class Ejercicio9 {
 		if (aux == 40) aux++;
 		else aux += 2;
 		Character cierre = (char)aux;
-		return cierre;
+		return cierre; //es la mejor idea que se me ocurrio para conseguir el contrario de un signo, no le den bola a las operaciones, le sumo al codigo ascii
 	}
 	private static boolean esBalanceado(String s) {
 		Stack<Character> pila = new Stack<>();
 		boolean ok = true;
 		
 		if ((s.length()%2 != 0 )||(s.length() > 0)&&(s.charAt(0) == ')')||(s.charAt(0) == ']')||(s.charAt(0) == '}')) ok = false;
+		//if ((s.length() > 0)&&(s.charAt(0) == ')')||(s.charAt(0) == ']')||(s.charAt(0) == '}')) ok = false;
+		//Esta es solo para epresion tipo: {[(a+b)*(c-d)*4]-[e/f]}
 		else {
 			int i = 0;
 			Character act, ape;
 			while (i<s.length() && ok) {
 				act = s.charAt(i);
 				if ((act == '(')||(act == '[')||(act == '{')) pila.push(act);
-				else if (!pila.isEmpty()) {
+				else if ((act == ')')||(act == ']')||(act == '}') && !pila.isEmpty()) {
 					ape = pila.pop();
 					if (AperturaACierre(ape) != act ) ok = false;
 				}
@@ -33,6 +35,7 @@ public class Ejercicio9 {
 		}
 		return ok;
 	}
+	
 	public static void main(String[] args) {
 		System.out.println("Ingrese una cadena de llaves, corchetes y parentesis");
 		Scanner sc = new Scanner(System.in);
